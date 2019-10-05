62d3c90f3e9bfb37cef08680a35251abec9ff781
unnhao <unnhao@gmail.com>
2019-10-05 19:07:07 +0800

    :zap: add flask-migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, escape, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 from config import app_config
 import datetime
 import os
@@ -9,4 +10,5 @@ import os
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[os.getenv('FLASK_ENV')]())
+    
     return app
