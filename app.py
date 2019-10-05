e090d68cefd83752ac527c4872cbc8291c0669a8
unnhao <unnhao@gmail.com>
2019-10-05 17:30:11 +0800

    :art: create_app structure

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, request, escape, abort
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 from config import app_config
-import datetime
+from app import create_app
 import os
 
-app = Flask(__name__)
-app.config.from_object(app_config[os.getenv('FLASK_ENV')]())
-
+app = create_app(app_config[os.getenv('FLASK_ENV')])
 
 @app.route('/')
 def hello():
     return 'Hello!Flask-Shop!'
+    
+if __name__=="__main__":
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 8787)))
